6a4f006109a69c2f54e96fede269ca1a8558e585
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-10-03 16:50:46 +0600

    connect with firebase

--- e_commerce_app/lib/main.dart
+++ e_commerce_app/lib/main.dart
@@ -1,8 +1,23 @@
+import 'dart:io';
+
 import 'package:e_commerce_app/views/buyers/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: FirebaseOptions(
+            apiKey: 'AIzaSyD5u_LN7pAzSZagZbzaTjrQPB8MnzXsrkU',
+            appId: '1:260677097183:android:8a58a1a1266e1e02b97b6a',
+            messagingSenderId: '260677097183',
+            projectId: 'e-commerce-app-26944',
+            storageBucket: 'gs://e-commerce-app-26944.appspot.com',
+          ),
+        )
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
